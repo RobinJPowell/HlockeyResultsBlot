5cd05388b32f67443c41c371bc7dbf1cde0b68b3
Robin Powell <RobinJPowell@users.noreply.github.com>
2023-08-29 11:21:24 +0100

    Add text for when Wal Carine has a fight

--- hlockeyResults.js
+++ hlockeyResults.js
@@ -2671,7 +2671,9 @@ async function finishStatsUpdate(weatherReportArray, miscCollection, statsCollec
 
                 if (walCarineGamesWithoutAFight > 0) {
                     weatherReport += `\nIt has been ${walCarineGamesWithoutAFight} games since Wal Carine has had a fight`;
-                }                
+                } else {
+                    weatherReport += 'Wal Carine\'s bloodlust has been sated';
+                }               
                     
                 // Discord limits messages to 2000 characters, so need to split this up
                 if (weatherReport.length > 2000) {
